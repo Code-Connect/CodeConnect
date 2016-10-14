fab8800bb63511f44da56fbfa24d46c3b12ed479
gap <khiemtonthat@yahoo.de>
2016-10-14 13:59:07 +0200

    test

---
+++ app/store/configureStore.js
@@ -0,0 +1,25 @@
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
+import rootReducer from '../reducers'
+
+//dunno what it does
+export default function configureStore(initialState) {
+  const logger = createLogger();
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk, promise, logger)
+  );
+
+  if (module.hot) {
+    // Enable hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers');
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return store;
+}
