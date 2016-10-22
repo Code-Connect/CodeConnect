a0a7d42f4638cf5b877e84becd854f0cf176f30d
gap <khiemtonthat@yahoo.de>
2016-10-22 12:46:37 +0200

    totalreset

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,36 +1,9 @@
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
-import { connect } from 'react-redux'
-import { logout } from '../actions/auth';
 
 class Header extends React.Component {
-  handleLogout(event) {
-    event.preventDefault();
-    this.props.dispatch(logout());
-  }
-
   render() {
     const active = { borderBottomColor: '#3f51b5' };
-    const rightNav = this.props.token ? (
-      <ul className="nav navbar-nav navbar-right">
-        <li className="dropdown">
-          <a href="#" data-toggle="dropdown" className="navbar-avatar dropdown-toggle">
-            <img src={this.props.user.picture || this.props.user.gravatar}/>
-            {' '}{this.props.user.name || this.props.user.email || this.props.user.id}{' '}
-            <i className="caret"></i>
-          </a>
-          <ul className="dropdown-menu">
-            <li><Link to="/account">My Account</Link></li>
-            <li className="divider"></li>
-            <li><a href="#" onClick={this.handleLogout.bind(this)}>Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-    ) : (
-      <ul className="nav navbar-nav navbar-right">
-        <li><Link to="/login" activeStyle={active}>Log in with Github</Link></li>
-      </ul>
-    );
     return (
       <nav className="navbar navbar-default navbar-static-top">
         <div className="container">
@@ -41,14 +14,13 @@ class Header extends React.Component {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <IndexLink to="/" className="navbar-brand">Code Connect</IndexLink>
+            <IndexLink to="/" className="navbar-brand">Project name</IndexLink>
           </div>
           <div id="navbar" className="navbar-collapse collapse">
             <ul className="nav navbar-nav">
               <li><IndexLink to="/" activeStyle={active}>Home</IndexLink></li>
               <li><Link to="/contact" activeStyle={active}>Contact</Link></li>
             </ul>
-            {rightNav}
           </div>
         </div>
       </nav>
@@ -56,11 +28,4 @@ class Header extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    token: state.auth.token,
-    user: state.auth.user
-  };
-};
-
-export default connect(mapStateToProps)(Header);
+export default Header;
