ff5a6e2cbe1baff3ae9a3b7575840901a5e26369
Gott50 <xxGott50xx@gmail.com>
2016-11-07 16:21:55 +0100

    add Tooltip as overlay to Buttons "Browse Projects" and "Add Project"

--- app/routes/homepage/UserButtons.js
+++ app/routes/homepage/UserButtons.js
@@ -1,5 +1,11 @@
 import React, {Component, PropTypes} from "react";
-import {Grid, Row, Col, Button} from "react-bootstrap";
+import {Grid, Row, Col, Button, OverlayTrigger, Tooltip} from "react-bootstrap";
+
+
+const browseProjectsTooltip = (<Tooltip id="tooltip"><strong>Looking for real-world Projects to work on?</strong>
+    These mentored Projects a what you seek</Tooltip>);
+const addProjectTooltip = (<Tooltip id="tooltip"><strong>Want your project to get worked on?</strong>
+    Add it to this AWESOME platform</Tooltip>);
 
 class UserButtons extends Component {
     render() {
@@ -12,23 +18,28 @@ class UserButtons extends Component {
                          style={ {
                              textAlign: 'right'
                          }}>
-                        <Button href="/contributor"
+                        <OverlayTrigger placement="left" overlay={browseProjectsTooltip}>
+                            <Button href="/contributor"
                                 bsStyle="success"
                                 style={ {paddingLeft: '6%', paddingRight: '6%'} }
                                 bsSize="large">
-                            Browse Projects
-                        </Button>
+                                Browse Projects
+                            </Button>
+                        </OverlayTrigger>
                     </Col>
                     <Col xs={ 6 }
                          md={ 6 }
                          sm={ 6 }
                          lg={ 6 }>
-                        <Button href="/mentor"
+
+                        <OverlayTrigger placement="right" overlay={addProjectTooltip}>
+                            <Button href="/mentor"
                                 bsStyle="warning"
                                 style={ {paddingRight: '10%', paddingLeft: '10%'} }
                                 bsSize="large">
-                            Add Project
-                        </Button>
+                                Add Project
+                            </Button>
+                        </OverlayTrigger>
                     </Col>
                 </Row>
             </Grid>
