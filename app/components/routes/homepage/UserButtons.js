731a71dcfd097d1c9474db2b74eeded3aac0a6f4
Gott50 <xxGott50xx@gmail.com>
2016-12-02 19:55:52 +0100

    disable UserButtons if not logged in

--- app/components/routes/homepage/UserButtons.js
+++ app/components/routes/homepage/UserButtons.js
@@ -6,32 +6,30 @@ import {Link} from "react-router";
 
 class UserButtons extends Component {
     buildBrowseProjectsTooltip() {
-        let message;
-        if (this.props.user)
-            message = (
+        let message = (this.props.user) ?
+            (
                 <div>
                     <strong>Looking for real-world Projects to work on? </strong>
                     These mentored Projects a what you seek
                 </div>
-            );
-        else message = (<strong>Please login to view all Projects</strong>);
+            ) : (<strong>Please login to view all Projects</strong>);
         return (<Tooltip id="tooltip">{message}</Tooltip>);
     }
 
     buildAddProjectTooltip() {
-        let message;
-        if (this.props.user)
-            message = (
+        let message = (this.props.user) ?
+            (
                 <div>
                     <strong>Want your project to get worked on? </strong>
                     Add it to this AWESOME platform
                 </div>
-            );
-        else message = (<strong>Please login add your Projects</strong>);
+            ) : (<strong>Please login add your Projects</strong>);
         return (<Tooltip id="tooltip">{message}</Tooltip>);
     }
 
     render() {
+        let toContributor = (this.props.user) ? "/contributor" : "/";
+        let toMentor = (this.props.user) ? '/mentor' : "/";
         return (
             <Grid fluid={true}>
                 <Row>
@@ -39,7 +37,7 @@ class UserButtons extends Component {
                         textAlign: 'right'
                     }}>
                         <OverlayTrigger placement="left" overlay={this.buildBrowseProjectsTooltip()}>
-                            <Link to="/contributor">
+                            <Link to={toContributor}>
                                 <Button bsStyle="success" style={{
                                     paddingLeft: '6%',
                                     paddingRight: '6%'
@@ -52,7 +50,7 @@ class UserButtons extends Component {
                     <Col xs={6} md={6} sm={6} lg={6}>
 
                         <OverlayTrigger placement="right" overlay={this.buildAddProjectTooltip()}>
-                            <Link to="/mentor">
+                            <Link to={toMentor}>
                                 <Button bsStyle="warning" style={{
                                 paddingRight: '10%',
                                 paddingLeft: '10%'
