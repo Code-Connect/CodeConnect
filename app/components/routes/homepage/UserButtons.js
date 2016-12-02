8bb20cadb55d01c3c86a7e9b0a4003ce7d6ffc2a
Gott50 <xxGott50xx@gmail.com>
2016-12-02 18:57:02 +0100

    replace href with Link

--- app/components/routes/homepage/UserButtons.js
+++ app/components/routes/homepage/UserButtons.js
@@ -1,12 +1,7 @@
 import React, {Component, PropTypes} from "react";
-import {
-    Grid,
-    Row,
-    Col,
-    Button,
-    OverlayTrigger,
-    Tooltip
-} from "react-bootstrap";
+import {connect} from "react-redux";
+import {Grid, Row, Col, Button, OverlayTrigger, Tooltip} from "react-bootstrap";
+import {Link} from "react-router";
 
 const browseProjectsTooltip = (
     <Tooltip id="tooltip">
@@ -30,23 +25,27 @@ class UserButtons extends Component {
                         textAlign: 'right'
                     }}>
                         <OverlayTrigger placement="left" overlay={browseProjectsTooltip}>
-                            <Button href="/contributor" bsStyle="success" style={{
-                                paddingLeft: '6%',
-                                paddingRight: '6%'
-                            }} bsSize="large">
-                                Browse Projects
-                            </Button>
+                            <Link to="/contributor">
+                                <Button bsStyle="success" style={{
+                                    paddingLeft: '6%',
+                                    paddingRight: '6%'
+                                }} bsSize="large">
+                                    Browse Projects
+                                </Button>
+                            </Link>
                         </OverlayTrigger>
                     </Col>
                     <Col xs={6} md={6} sm={6} lg={6}>
 
                         <OverlayTrigger placement="right" overlay={addProjectTooltip}>
-                            <Button href="/mentor" bsStyle="warning" style={{
+                            <Link to="/mentor">
+                                <Button bsStyle="warning" style={{
                                 paddingRight: '10%',
                                 paddingLeft: '10%'
                             }} bsSize="large">
                                 Add Project
                             </Button>
+                            </Link>
                         </OverlayTrigger>
                     </Col>
                 </Row>
@@ -55,4 +54,10 @@ class UserButtons extends Component {
     }
 
 }
-export default UserButtons;
+const mapStateToProps = (state) => {
+    console.log("mapStateToProps");
+    console.log(state.user.github);
+    return {user: state.user.github};
+};
+
+export default connect(mapStateToProps)(UserButtons);
