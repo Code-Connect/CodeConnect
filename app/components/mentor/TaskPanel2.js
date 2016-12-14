18c5ad7590f6f98d4d2681c3a5bf1c5ad1781204
gap <khiemtonthat@yahoo.de>
2016-12-14 18:13:30 +0100

    added TestPanel2

---
+++ app/components/mentor/TaskPanel2.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+    Panel,
+    Form,
+    FormGroup,
+    ControlLabel,
+    FormControl,
+    Button,
+    Col
+} from "react-bootstrap";
+
+class TaskPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: '',
+            description: ''
+        }
+    }
+
+    addTask(event) {
+        event.preventDefault();
+        this.props.addTask;
+    }
+
+    handleChange(event) {
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+        console.log(this.state);
+        //this.props.handleChange;
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <button className="list-group-item" onClick={this.props.handleChange}>{this.props.projects.name}
+                    </button>
+                </div>
+                <Panel collapsible expanded={true}>
+                    <Form horizontal>
+                        <FormGroup name="task">
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Task
+                            </Col>
+                            <Col sm={10}>
+                                <FormControl type="text" name="task" placeholder="Task" onChange={this.handleChange.bind(this)}/>
+                            </Col>
+                        </FormGroup>
+
+                        <FormGroup name="description">
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Description
+                            </Col>
+                            <Col sm={10}>
+                                <FormControl type="text" name="description" placeholder="Description" onChange={this.handleChange.bind(this)}/>
+                            </Col>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Col smOffset={2} sm={10}>
+                                <Button type="submit" name="submit" repoid={this.props.repoid} onClick={this.props.addTask}>
+                                    Submit
+                                </Button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </Panel>
+            </div>
+        )
+    }
+}
+
+TaskPanel.propTypes = {
+    addTask: React.PropTypes.func,
+    projects: React.PropTypes.shape({description: React.PropTypes.string, name: React.PropTypes.string, repoid: React.PropTypes.number, repourl: React.PropTypes.string})
+}
+
+TaskPanel.defaultProps = {
+    projects: {
+        description: 'No Description'
+    }
+}
+
+export default TaskPanel;
