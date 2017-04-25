41f6db369644d4e52e1487b94b6a6efa14e29d04
gap <khiemtonthat@yahoo.de>
2017-04-25 16:12:51 +0200

    changed colors of the tasks blocks. commented unused code out

--- app/components/routes/HomePage.js
+++ app/components/routes/HomePage.js
@@ -10,7 +10,7 @@ class HomePage extends Component {
             <div>
                 <Logo/>
                 <UserButtons/>
-                <NewestProjects/>
+                {/* <NewestProjects/> */}
             </div>
         );
     }
