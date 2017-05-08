196a109fa8d9cabbb9b1d53dc65afc63fbbae04d
gap <khiemtonthat@yahoo.de>
2017-05-09 00:58:53 +0200

    added redux debug addon

--- app/components/baukasten/TaskPanelCol.js
+++ app/components/baukasten/TaskPanelCol.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Panel, Button, Col} from "react-bootstrap";
 import ReactMarkdown from 'react-markdown';
+import SkyLight from 'react-skylight';
+import EditPanel from './EditPanel';
 
 //used in TaskBlock
 class TaskPanelCol extends React.Component {
@@ -9,7 +11,20 @@ class TaskPanelCol extends React.Component {
       <Col xs={6} md={4}>
         <Panel header={(
           <div>
-            <Button className="pull-right" bsStyle="primary" bsSize="small" name="edit" onClick={this.props.function}>Edit</Button>
+            <div>
+              <Button className="pull-right" bsStyle="primary" bsSize="small" name="edit" onClick={() => this.refs.simpleDialog.show()}>Edit</Button>
+              <SkyLight dialogStyle={{
+                backgroundColor: '#00897B',
+                color: '#ffffff',
+                width: '70%',
+                height: '600px',
+                marginTop: '-300px',
+                marginLeft: '-35%'
+              }} hideOnOverlayClicked ref="simpleDialog" title="Hi, I'm a simple modal">
+                Hello, I dont have any callback.
+                <EditPanel/>
+              </SkyLight>
+            </div>
             <h3>{this.props.panelType}</h3>
           </div>
         )} bsStyle="success" onClick= {()=>{}}>
--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -7,13 +7,15 @@ import thunk from 'redux-thunk'
 import promise from 'redux-promise';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 export default function configureStore(initialState) {
     const logger = createLogger();
     const store = createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk, promise, logger)
+        composeWithDevTools(applyMiddleware(thunk, promise, logger))
     );
 
     if (module.hot) {
