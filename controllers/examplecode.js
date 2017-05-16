6087f96a7f7760cf348f756713802ca44ab54d89
gap <khiemtonthat@yahoo.de>
2017-05-16 09:44:00 +0200

    bug fix

---
+++ controllers/examplecode.js
@@ -0,0 +1,26 @@
+var Projectmodel = require('../models/Project');
+var Project = Projectmodel.Project;
+
+exports.getUser = function() {
+    new Project({
+        project_id: "sdfa"
+    }).fetch().then(function(model) {
+        if (!model) {
+            new Project({
+                project_id: "sdfa", //if project is not there create one
+                name: "fdasdf",
+                data: {
+                    "data1": "ewfew",
+                    "data2": "hallo"
+                }
+            }).save(null, { //save project in database
+                method: 'insert'
+            }).then(function() { //then answer
+                res.send("bäm")
+            });
+        } else {
+            console.log(model.attributes.data);
+            return model.attributes.data;
+        }
+    })
+}
