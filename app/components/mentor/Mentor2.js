1bf7f8a00a1983742ceb8554ea563517c74bbb18
gap <khiemtonthat@yahoo.de>
2017-05-17 14:39:07 +0200

    der Code wurde nun in eine seprate component verlagert

--- app/components/mentor/Mentor2.js
+++ app/components/mentor/Mentor2.js
@@ -5,28 +5,14 @@ import ReactMarkdown from 'react-markdown';
 import EditPanel from '../baukasten/EditPanel.js';
 import Editor from '../baukasten/Editor.js';
 import {postTask} from '../../actions/taskActions';
+import TaskPanel from './TaskPanel';
 
 class Mentor2 extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      input: false,
-      output: false,
-      description: false
-    }
-  }
-
-  toggleButton(event) {
-    this.setState({
-      [event.target.name]: !this.state[event.target.name]
-    }, () => {
-      console.log(this.state)
-    });
   }
 
   saveChange(event) {
-    this.toggleButton(event);
-    // TODO save the text in the database
     this.props.dispatch(postTask(event.target.id, event.target.name, event.target.value));
   }
 
@@ -34,56 +20,16 @@ class Mentor2 extends React.Component {
     this.props.dispatch({type: 'UPDATE_TEXT', task_id: task_id, fieldtype: fieldtype, newCode: newCode});
   }
 
-  createPanel(fieldtype, task) {
-    const editPanel = this.state[fieldtype]
-      ?// fieldtype: input or description or output
-      (<Editor onChange={this.updateText.bind(this)} task_id={task.task_id} fieldtype={fieldtype} code={task[fieldtype]}/>)
-      : null;
-      
-    const editOrSaveButton = this.state[fieldtype]
-      ? <Button id={task.task_id} name={fieldtype} value = {task[fieldtype]} className="pull-right" onClick={this.saveChange.bind(this)}>Save</Button>
-      : <Button name={fieldtype} className="pull-right" onClick={this.toggleButton.bind(this)}>Edit</Button>
-
-    return (
-      <div>
-        <div>
-          <Panel header={(
-            <div>
-              {editOrSaveButton}
-              <h4>{fieldtype}</h4>
-            </div>
-          )}>
-            <ReactMarkdown source={task[fieldtype]}/> {editPanel}
-          </Panel>
-        </div>
-      </div>
-    )
-  }
-
   render() {
     return (
       <div>
         {this.props.tasks.map((task) => {
-          return (
-            <Panel header={< h1 > {
-              task.name
-            } < /h1>} bsStyle="warning">
-              {this.createPanel("input", task)}
-              {this.createPanel("output", task)}
-              {this.createPanel("description", task)}
-            </Panel>
-          )
+          return (<TaskPanel task={task} updateText={this.updateText.bind(this)} saveChange={this.saveChange.bind(this)}/>)
         })}
       </div>
     );
   }
 }
-
-// tasks[0] = {input: string,
-//         output: string,
-//         description: string,
-//         task_id: integer,
-//         name: string}
 
 const mapStateToProps = (state) => {
   return {tasks: state.tasks.mockData};
--- app/components/mentor/TaskPanel.js
+++ app/components/mentor/TaskPanel.js
@@ -1,75 +1,83 @@
 import React from 'react';
-import {
-    Panel,
-    Form,
-    FormGroup,
-    ControlLabel,
-    FormControl,
-    Button,
-    Col
-} from "react-bootstrap";
+import {connect} from 'react-redux'
+import {Panel, Button} from "react-bootstrap";
+import ReactMarkdown from 'react-markdown';
+import Editor from '../baukasten/Editor.js';
+import {postTask} from '../../actions/taskActions';
 
-const Mentor2 = React.createClass({
-    getInitialState() {
-        return this.state = {
-            task: '',
-            description: ''
-        }
-    },
-
-    propTypes: {
-        addTask: React.PropTypes.func,
-        handleChange: React.PropTypes.func,
-        projects: React.PropTypes.shape({description: React.PropTypes.string, name: React.PropTypes.string, project_id: React.PropTypes.number, repourl: React.PropTypes.string})
-    },
-
-    getDefaultProps() {
-        return {
-            projects: {
-                description: 'No Description'
-            }
-        };
-    },
-
-    render() {
-        return (
-            <div>
-                <div>
-                    <button className="list-group-item" onClick={this.props.handleChange}>{this.props.projects.name}
-                    </button>
-                </div>
-                <Panel collapsible expanded={true}>
-                    <Form horizontal>
-                        <FormGroup name="task">
-                            <Col componentClass={ControlLabel} sm={2}>
-                                Task
-                            </Col>
-                            <Col sm={10}>
-                                <FormControl type="text" name="task" placeholder="Task" id={this.props.projects.project_id} onChange={this.props.handleChange}/>
-                            </Col>
-                        </FormGroup>
-
-                        <FormGroup name="description">
-                            <Col componentClass={ControlLabel} sm={2}>
-                                Description
-                            </Col>
-                            <Col sm={10}>
-                                <FormControl type="text" name="description" placeholder="Description" id={this.props.projects.project_id} onChange={this.props.handleChange}/>
-                            </Col>
-                        </FormGroup>
-
-                        <FormGroup>
-                            <Col smOffset={2} sm={10}>
-                                <Button type="submit" name="submit" project_id={this.props.project_id} onClick={this.props.addTask}>
-                                    Submit
-                                </Button>
-                            </Col>
-                        </FormGroup>
-                    </Form>
-                </Panel>
-            </div>
-        )
+class TaskPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: false,
+      output: false,
+      description: false,
+      task: this.props.task
     }
-})
+  }
 
-export default TaskPanel;
+  toggleButton(event) {
+    this.setState({
+      [event.target.name]: !this.state[event.target.name]
+    }, () => {
+      console.log(this.state)
+    });
+  }
+
+  saveChange(event) {
+    this.toggleButton(event);
+    // TODO hier muss das gedispatcht werden
+    // this.props.dispatch(postTask(event.target.id, event.target.name, event.target.value));
+    this.props.saveChange(event);
+  }
+
+  updateText(task_id, fieldtype, newCode) {
+    this.props.updateText(task_id, fieldtype, newCode);
+  }
+
+  createPanel(fieldtype, task) {
+    const editPanel = this.state[fieldtype]
+      ? // fieldtype: input or description or output
+      (<Editor onChange={this.updateText.bind(this)} task_id={task.task_id} fieldtype={fieldtype} code={task[fieldtype]}/>)
+      : null;
+
+    const editOrSaveButton = this.state[fieldtype]
+      ? <Button id={task.task_id} name={fieldtype} value={task[fieldtype]} className="pull-right" onClick={this.saveChange.bind(this)}>Save</Button>
+      : <Button name={fieldtype} className="pull-right" onClick={this.toggleButton.bind(this)}>Edit</Button>
+
+    return (
+      <div>
+        <Panel header={(
+          <div>
+            {editOrSaveButton}
+            <h4>{fieldtype}</h4>
+          </div>
+        )}>
+          <ReactMarkdown source={this.props.task[fieldtype]}/> {editPanel}
+        </Panel>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <Panel header={< h1 > {
+          this.props.task.name
+        } < /h1>} bsStyle="warning">
+          {this.createPanel("input", this.props.task)}
+          {this.createPanel("output", this.props.task)}
+          {this.createPanel("description", this.props.task)}
+        </Panel>
+      </div>
+    );
+  }
+}
+
+// tasks[0] = {input: string,
+//         output: string,
+//         description: string,
+//         task_id: integer,
+//         name: string}
+
+export default(TaskPanel);
