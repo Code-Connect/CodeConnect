d80cb2e7e057ac0399fe80f06f353249876c6aa9
gap <khiemtonthat@yahoo.de>
2017-05-21 01:36:09 +0200

    inputfield for name

--- app/components/mentor/Mentor2.js
+++ app/components/mentor/Mentor2.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import {Panel, Button} from "react-bootstrap";
+import {Panel, Button, FormGroup, InputGroup, FormControl} from "react-bootstrap";
 import ReactMarkdown from 'react-markdown';
 import EditPanel from '../baukasten/EditPanel.js';
 import Editor from '../baukasten/Editor.js';
@@ -10,6 +10,9 @@ import TaskPanel from './TaskPanel';
 class Mentor2 extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      inputfield: ''
+    };
   }
 
   saveChange(event) {
@@ -20,15 +23,26 @@ class Mentor2 extends React.Component {
     this.props.dispatch({type: 'UPDATE_TEXT', task_id: task_id, fieldtype: fieldtype, newCode: newCode});
   }
 
-  addTask(event){
+  addTask(event) {
     // TODO muss noch gemacht werden
-    this.props.dispatch(addTask("1 kewin"));
+    this.props.dispatch(addTask(this.state.inputfield));
+  }
+
+  handleInputChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
     return (
       <div>
-        <Button bsStyle = "success" onClick = {this.addTask.bind(this)} >Add new Task</Button>
+        <FormGroup>
+          <InputGroup>
+            <FormControl type="text" name = "inputfield" placeholder="Projectname" value={this.state.inputfield} onChange={this.handleInputChange.bind(this)}/>
+            <InputGroup.Button>
+              <Button bsStyle="success" onClick={this.addTask.bind(this)}>Add new Task</Button>
+            </InputGroup.Button>
+          </InputGroup>
+        </FormGroup>
 
         {this.props.tasks.map((task) => {
           return (<TaskPanel task={task} updateText={this.updateText.bind(this)} saveChange={this.saveChange.bind(this)}/>)
