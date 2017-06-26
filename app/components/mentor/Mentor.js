00c17edd3657304859c3887cb4826f86b3bc08dd
gap <khiemtonthat@yahoo.de>
2017-06-26 23:24:13 +0200

    get projects from orgs and from user

--- app/components/mentor/Mentor.js
+++ app/components/mentor/Mentor.js
@@ -1,73 +1,41 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import {getReposGithub, addProjectsToCodeConnect, postProject, postTasksToProject} from './../../actions/mentor';
-import {Panel} from "react-bootstrap";
-import TaskPanel from "./TaskPanel2";
+import {Panel, Button} from "react-bootstrap";
+// import {getReposGithub, addProjectsToCodeConnect, postProject, postTasksToProject} from './../../actions/mentor';
+import {getOrgsProject, getUserProject} from './../../actions/projectActions';
+
 
 class Mentor extends React.Component {
     constructor(props) {
         super(props);
     }
 
-    handleSubmit(event) {
-        this.props.dispatch(postProject(this.props.ccrepos, this.props.id));
-    }
+    onClick(event){
+      console.log(event.target);
+      this.props.dispatch(getOrgsProject(this.props.user.token));
+      this.props.dispatch(getUserProject(this.props.user.token));
 
-    addProject(project) {
-        this.props.dispatch(addProjectsToCodeConnect(project));
-    }
-
-    addTask(state, ccrepo_id) {
-        this.props.dispatch(postTasksToProject(ccrepo_id, state, this.props.id));
     }
 
     //gets called, when the component gets loaded
     componentDidMount() {
         //gets the repos from github
-        this.props.dispatch(getReposGithub('https://api.github.com/users/' + this.props.github_name + '/repos'));
+        this.props.dispatch(getReposGithub('https://api.github.com/user/orgs'));
     }
 
     render() {
         return (
             <div>
-                <div className="container">
-                    <div className="panel">
-                        <div className="panel-heading">
-                            <h3 className="panel-title">Not Added Projects</h3>
-                        </div>
-                        <div className="panel-body">
-                            <ul className="list-group">
-                                {this.props.repos.map((item) => {
-                                    return (
-                                        <div>
-                                            <button className="list-group-item" onClick={() => this.addProject(item)}>{item.name}</button>
-                                        </div>
-                                    );
-                                })}
-                            </ul>
-                            <button className="btn btn-success" onClick={this.handleSubmit.bind(this)}>Update Project</button>
-                        </div>
-
-                        <div className="panel-heading">
-                            <h3 className="panel-title">Your Code Connect Projects</h3>
-                        </div>
-
-                        <div className="panel-body">
-                            <ul className="list-group">
-                                {this.props.ccrepos.map((item, index) => {
-                                    return (<TaskPanel projects={item} addTask={this.addTask.bind(this)}/>);
-                                })}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+              <Button onClick={this.onClick.bind(this)}>
+                Press me
+              </Button>
             </div>
         );
     }
 }
 
 const mapStateToProps = (state) => {
-    return {repos: state.projects.repos, ccrepos: state.projects.ccrepos, id: state.user.github.id, github_name: state.user.github.name};
+    return {user: state.user.github};
 };
 
 export default connect(mapStateToProps)(Mentor);
--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {IndexRoute, Route} from "react-router";
 import App from "./components/App";
-import Mentor from "./components/mentor/Mentor2";
+import MentorTask from "./components/mentor/Mentor2";
+import Mentor from "./components/mentor/Mentor";
 import NotFound from "./components/NotFound";
 import HomePage from "./components/HomePage";
 import Contributor from "./components/contributor/Contributor";
@@ -10,19 +11,20 @@ import Feedback from "./components/feedback/Feedback";
 
 export default function getRoutes(store) {
 
-    const clearMessages = () => {
-        store.dispatch({type: 'CLEAR_MESSAGES'});
-    };
+  const clearMessages = () => {
+    store.dispatch({type: 'CLEAR_MESSAGES'});
+  };
 
-    return (
-        <Route path="/" component={App}>
-            <IndexRoute component={HomePage} onLeave={clearMessages}/>
-            <Route path="/mentor" component={Mentor} onLeave={clearMessages}/>
-            <Route path="/home" component={HomePage} onLeave={clearMessages}/>
-            <Route path="/contributor" component={Contributor} onLeave={clearMessages}/>
-            <Route path="/editor" component={Editor} onLeave={clearMessages}/>
-            <Route path="/feedback" component={Feedback} onLeave={clearMessages}/>
-            <Route path="*" component={NotFound} onLeave={clearMessages}/>
-        </Route>
-    );
+  return (
+    <Route path="/" component={App}>
+      <IndexRoute component={HomePage} onLeave={clearMessages}/>
+      <Route path="/mentor/task" component={MentorTask} onLeave={clearMessages}/>
+      <Route path="/mentor" component={Mentor} onLeave={clearMessages}/>
+      <Route path="/home" component={HomePage} onLeave={clearMessages}/>
+      <Route path="/contributor" component={Contributor} onLeave={clearMessages}/>
+      <Route path="/editor" component={Editor} onLeave={clearMessages}/>
+      <Route path="/feedback" component={Feedback} onLeave={clearMessages}/>
+      <Route path="*" component={NotFound} onLeave={clearMessages}/>
+    </Route>
+  );
 }
