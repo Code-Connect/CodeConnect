1c48fc7322970cb42cad35bac8c9b851d8dc10ff
Gott50 <xxGott50xx@gmail.com>
2017-06-30 17:53:08 +0200

    add Tutorial Text

--- app/components/homepage/ContributorTutorial.js
+++ app/components/homepage/ContributorTutorial.js
@@ -1,10 +1,16 @@
 import React, {Component} from "react";
 import TryGitModal from "./TryGitModal";
+import InstallGit from "./InstallGit";
 
 class ContributorTutorial extends Component {
     render() {
         return (
-            <div>
+            <div className="tutorial center-block">
+                <h2>1. GitHub</h2>
+                <p>Every projects on CodeConnect uses Git as Version Control System (VCS). GitHub is the biggest
+                    platform to put SourceCode in the open. Hence the term OpenSource. Now it is your turn to share Code
+                    and contribute on Projects. The following steps will show you how.</p>
+                <InstallGit/>
                 <TryGitModal/>
             </div>
         );
---
+++ app/components/homepage/InstallGit.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class InstallGit extends Component {
+    render() {
+        return (
+            <div>
+                install
+            </div>
+        );
+    }
+}
+
+export default InstallGit;
