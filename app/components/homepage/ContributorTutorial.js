a26355c8c2df679bf03d4f5f6903221a2432cc7a
Gott50 <xxGott50xx@gmail.com>
2017-07-01 10:09:55 +0200

    add Contributor Tutorial: 2. CodeConnect

--- app/components/homepage/ContributorTutorial.js
+++ app/components/homepage/ContributorTutorial.js
@@ -31,7 +31,13 @@ class ContributorTutorial extends Component {
                 <h2>2. CodeConnect</h2>
                 <p>Now that you got Git and know about GitHub. Let's work together to make OpenSource great again.</p>
                 <ol>
-                    <li>Navigate to the <a>Contributor</a> Section</li>
+                    <li>Navigate to the <a href="/contributor">Contributor</a> Section</li>
+                    <li>Choose a Project/Task to work on</li>
+                    <li>Clone it to your Machine</li>
+                    <li>Start Coding</li>
+                    <li>Create PullRequest</li>
+                    <li>Give Feedback</li>
+                    <li>Receive for Feedback</li>
                 </ol>
             </div>
         );
