836f11b8d36053e8d8355d03012a0aaa15af7012
Gott50 <xxGott50xx@gmail.com>
2017-07-01 10:59:26 +0200

    add Installing Git on Mac

--- app/components/homepage/InstallGit.js
+++ app/components/homepage/InstallGit.js
@@ -16,7 +16,40 @@ class InstallGit extends Component {
                         </Col>
                         <Col md={4}>
                             <TutorialModal title={"How to install Git on Mac"} button={"Mac"}>
-                                Coming soon ;)
+                                <div>
+                                    <h3 id="_installing_on_mac">Installing on Mac</h3>
+                                    <div>
+                                        <p>
+                                            There are several ways to install Git on a Mac.
+                                            The easiest is probably to install the Xcode Command Line Tools.
+                                            On Mavericks (10.9) or above you can do this simply by trying to run
+                                            <em>git</em> from the Terminal the very first time.
+                                            If you don’t have it installed already, it will prompt you to install it.
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <p>If you want a more up to date version, you can also install it via a binary
+                                            installer.
+                                            An OSX Git installer is maintained and available for download at the Git
+                                            website, at <a href="http://git-scm.com/download/mac">http://git-scm.com/download/mac</a>.
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <img src="https://git-scm.com/book/en/v2/images/git-osx-installer.png"
+                                                 alt="Git OS X installer."/>
+                                        </div>
+                                        <div>Figure 7. Git OS X Installer.</div>
+                                    </div>
+                                    <div>
+                                        <p>You can also install it as part of the GitHub for Mac install.
+                                            Their GUI Git tool has an option to install command line tools as well.
+                                            You can download that tool from the GitHub for Mac website, at <a
+                                                href="http://mac.github.com">http://mac.github.com</a>.</p>
+                                    </div>
+                                </div>
+                                <a target="_blank"
+                                   href="https://git-scm.com/book/en/v2/Getting-Started-Installing-Git/#_installing_on_mac">Source</a>
                             </TutorialModal>
                         </Col>
                         <Col md={4}>
