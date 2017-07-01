47248c2909dc459a10a5b665b0029d90ed6b20ef
Gott50 <xxGott50xx@gmail.com>
2017-07-01 10:54:44 +0200

    add Installing Git on Linux

--- app/components/homepage/InstallGit.js
+++ app/components/homepage/InstallGit.js
@@ -21,7 +21,37 @@ class InstallGit extends Component {
                         </Col>
                         <Col md={4}>
                             <TutorialModal title={"How to install Git on Linux"} button={"Linux"}>
-                                Coming soon ;)
+                                <div>
+                                    <h3>Installing on Linux</h3>
+                                    <div>
+                                        <p>
+                                            If you want to install the basic Git tools on Linux via a binary installer,
+                                            you can generally do so through the basic package-management tool that comes
+                                            with your distribution.
+                                            If you’re on Fedora for example, you can use dnf:</p>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <pre><code data-lang="console">$ sudo dnf install git-all</code></pre>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <p>If you’re on a Debian-based distribution like Ubuntu, try apt-get:</p>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <pre><code data-lang="console">$ sudo apt-get install git-all</code></pre>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <p>For more options, there are instructions for installing on several different
+                                            Unix flavors on the Git website, at <a
+                                                href="http://git-scm.com/download/linux">http://git-scm.com/download/linux</a>.
+                                        </p>
+                                    </div>
+                                </div>
+                                <a target="_blank"
+                                   href="https://git-scm.com/book/en/v2/Getting-Started-Installing-Git/#_installing_on_linux">Source</a>
                             </TutorialModal>
                         </Col>
                     </Row>
