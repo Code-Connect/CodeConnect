881b64e82e233ca6b5b5e010bd1377d086b3a317
Gott50 <xxGott50xx@gmail.com>
2017-07-11 20:21:02 +0200

    Fix Error: knex: Required configuration option 'client' is missing.
        2017-07-11T18:16:31.601776+00:00 app[web.1]:     at new Client (/app/node_modules/knex/lib/client.js:108:11)
    
        2017-07-11T18:16:31.601777+00:00 app[web.1]:     at Knex (/app/node_modules/knex/lib/index.js:60:34)
    
        2017-07-11T18:16:31.601777+00:00 app[web.1]:     at Object.<anonymous> (/app/server.js:66:14)
    
        2017-07-11T18:16:31.601778+00:00 app[web.1]:     at Module._compile (module.js:541:32)
    
        2017-07-11T18:16:31.601778+00:00 app[web.1]:     at Object.Module._extensions..js (module.js:550:10)
    
        2017-07-11T18:16:31.601779+00:00 app[web.1]:     at Module.load (module.js:456:32)
    
        2017-07-11T18:16:31.601780+00:00 app[web.1]:     at tryModuleLoad (module.js:415:12)
    
        2017-07-11T18:16:31.601780+00:00 app[web.1]:     at Function.Module._load (module.js:407:3)
    
        2017-07-11T18:16:31.601781+00:00 app[web.1]:     at Function.Module.runMain (module.js:575:10)
    
        2017-07-11T18:16:31.601781+00:00 app[web.1]:     at startup (node.js:160:18)
    
        2017-07-11T18:16:31.601782+00:00 app[web.1]:     at node.js:445:3

--- config/bookshelf.js
+++ config/bookshelf.js
@@ -1,5 +1,6 @@
+let env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
 var config = require('../knexfile');
-var knex = require('knex')(config);
+var knex = require('knex')(config[env]);
 var bookshelf = require('bookshelf')(knex);
 
 //bookshelf.plugin('virtuals');
