b709b21a23b02b08e0af4a81dd68a23381d6bfc3
Gott50 <xxGott50xx@gmail.com>
2017-07-11 20:15:29 +0200

    Fix Error: ENOENT: no such file or directory, open '.env'
        2017-07-11T18:09:20.632243+00:00 app[web.1]:     at Error (native)
    
        2017-07-11T18:09:20.632250+00:00 app[web.1]:     at Object.fs.openSync (fs.js:634:18)
    
        2017-07-11T18:09:20.632251+00:00 app[web.1]:     at Object.fs.readFileSync (fs.js:502:33)
    
        2017-07-11T18:09:20.632256+00:00 app[web.1]:     at Object.module.exports.config (/app/node_modules/dotenv/lib/main.js:30:37)
    
        2017-07-11T18:09:20.632256+00:00 app[web.1]:     at Object.<anonymous> (/app/knexfile.js:3:8)
    
        2017-07-11T18:09:20.632257+00:00 app[web.1]:     at Module._compile (module.js:541:32)
    
        2017-07-11T18:09:20.632262+00:00 app[web.1]:     at loader (/app/node_modules/babel-register/lib/node.js:144:5)
    
        2017-07-11T18:09:20.632270+00:00 app[web.1]:     at Object.require.extensions.(anonymous function) [as .js] (/app/node_modules/babel-register/lib/node.js:154:7)
    
        2017-07-11T18:09:20.632270+00:00 app[web.1]:     at Module.load (module.js:456:32)
    
        2017-07-11T18:09:20.632271+00:00 app[web.1]:     at tryModuleLoad (module.js:415:12)
    
        2017-07-11T18:09:20.632271+00:00 app[web.1]:     at Function.Module._load (module.js:407:3)
    
        2017-07-11T18:09:20.632271+00:00 app[web.1]:     at Module.require (module.js:466:17)
    
        2017-07-11T18:09:20.632272+00:00 app[web.1]:     at require (internal/module.js:20:19)
    
        2017-07-11T18:09:20.632272+00:00 app[web.1]:     at Object.<anonymous> (/app/server.js:66:19)
    
        2017-07-11T18:09:20.632273+00:00 app[web.1]:     at Module._compile (module.js:541:32)
    
        2017-07-11T18:09:20.632273+00:00 app[web.1]:     at Object.Module._extensions..js (module.js:550:10)
    
        2017-07-11T18:09:20.632273+00:00 app[web.1]:     at Module.load (module.js:456:32)
    
        2017-07-11T18:09:20.632274+00:00 app[web.1]:     at tryModuleLoad (module.js:415:12)
    
        2017-07-11T18:09:20.632274+00:00 app[web.1]:     at Function.Module._load (module.js:407:3)
    
        2017-07-11T18:09:20.632274+00:00 app[web.1]:     at Function.Module.runMain (module.js:575:10)
    
        2017-07-11T18:09:20.632275+00:00 app[web.1]:     at startup (node.js:160:18)
    
        2017-07-11T18:09:20.632275+00:00 app[web.1]:     at node.js:445:3 errno: -2, code: 'ENOENT', syscall: 'open', path: '.env' }

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,23 @@
-var dotenv = require('dotenv');
-
-dotenv.load();
-
-// module.exports = {
-//   client: 'pg',
-//   connection: process.env.DATABASE_URL
-// };
+// Load environment variables from .env file
+if (process.env.NODE_ENV !== "production")
+    loadEnvironmentVariables();
+function loadEnvironmentVariables() {
+    let dotenv = require('dotenv');
+    dotenv.load();
+}
 
 module.exports = {
-  client: 'pg',
-  connection: {
-    host : 'localhost',
-    user : process.env.LOCALDATABASE_USER,
-    password :  process.env.LOCALDATABASE_PASSWORD,
-    database : 'postgres'
-  }
+    development: {
+        client: 'pg',
+        connection: {
+            host : 'localhost',
+            user : process.env.LOCALDATABASE_USER,
+            password :  process.env.LOCALDATABASE_PASSWORD,
+            database : 'postgres'
+        }
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL
+    }
 };
