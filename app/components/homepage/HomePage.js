421934eb42a1e4088517b83333ac469aa5689d57
Gott50 <xxGott50xx@gmail.com>
2017-07-16 18:15:41 +0200

    show About when not logged in

--- app/components/homepage/HomePage.js
+++ app/components/homepage/HomePage.js
@@ -1,14 +1,24 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
 import About from "../about/About";
 
 class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        const page = this.props.user ? <p>Logged in</p> : <About/>;
         return (
             <div className="HomePage">
-                <About/>
+                {page}
             </div>
         );
     }
 }
 
-export default HomePage;
+const mapStateToProps = (state) => {
+    return {user: state.user.github};
+};
+
+export default connect(mapStateToProps)(HomePage);
