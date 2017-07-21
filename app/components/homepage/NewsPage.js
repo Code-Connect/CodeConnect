2cfc3662fe3fce7412063afaa5ad000d6ce1701f
Gott50 <xxGott50xx@gmail.com>
2017-07-21 23:21:08 +0200

    add ProjectTableView to NewsPage

--- app/components/homepage/NewsPage.js
+++ app/components/homepage/NewsPage.js
@@ -1,10 +1,11 @@
 import React, {Component} from "react";
+import ProjectTableView from "../baukasten/ProjectTableView";
 
 class NewsPage extends Component {
     render() {
         return (
-            <div>
-                News
+            <div className="news-page">
+                <ProjectTableView/>
             </div>
         );
     }
