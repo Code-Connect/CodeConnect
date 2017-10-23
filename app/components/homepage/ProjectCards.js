e9b81ab4b92585d3a9e9f943fbe9d9f731e3198a
gap <khiemtonthat@yahoo.de>
2017-10-24 00:48:39 +0200

    added ProjectCards

---
+++ app/components/homepage/ProjectCards.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {connect} from 'react-redux'
+import {Button, Panel, Table} from "react-bootstrap";
+import ReactMarkdown from 'react-markdown';
+
+
+class ProjectCards extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const description = this.props.project.description
+      ? this.props.project.description
+      : "";
+    return (
+      <div>
+        <div className="headerWrapper">
+          <h3>{this.props.project.name}</h3>
+        </div>
+        <br/>
+        <br/>
+        <ReactMarkdown source={this.props.project.description}/>
+        <div className="btn-wrapper">
+          <span className="btn btn-default newsPageProjektButton">To Project</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProjectCards;
