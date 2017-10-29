e889d2edbfc54ce0aae085f57aa53d62d17a7fd4
gap <khiemtonthat@yahoo.de>
2017-10-29 14:45:10 +0100

    fixed bug renamed CLEARNPRJECTLIST INTO GETPROJECT_LISTSUCCESSFUL

--- app/reducers/currentProjectList.js
+++ app/reducers/currentProjectList.js
@@ -1,12 +1,12 @@
 export default function messages(state = {}, action) {
   switch (action.type) {
-    case 'CLEAR_PROJECT_LIST':
+    case 'GET_PROJECT_LIST_SUCCESSFUL':
       return Object.assign({}, {
         projectList: action.projectList,
         error: null,
         loading: false
       });
-    case 'GET_PROJECT_LIST_SUCCESSFUL':
+    case 'CLEAR_PROJECT_LIST':
       return {projectList: [], error: null, loading: true};
     case 'GET_PROJECT_LIST_ERROR':
       return {projectList: [], error: null, loading: true};
