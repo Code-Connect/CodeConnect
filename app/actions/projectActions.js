de404a46daf283fca0b37879414cf936bf62bbbe
gap <khiemtonthat@yahoo.de>
2017-10-30 22:41:13 +0100

    delted TODOs and commented the old gunction getOrgsProject and getUSERProject

--- app/actions/projectActions.js
+++ app/actions/projectActions.js
@@ -2,61 +2,61 @@ import 'whatwg-fetch';
 
 // gets the github repos as array
 // @param {string} githublink
-export function getOrgsProject(token) {
-  return (dispatch) => {
-    return fetch('https://api.github.com/user/orgs', {
-      method: 'get',
-      headers: {
-        'Authorization': 'token ' + token
-      }
-    }).then((response) => {
-      return response.json().then(function(orgs) {
-        var repoarray = [];
-        Promise.all(orgs.map((item) => {
-          return fetch(item.repos_url).then((response2) => {
-            return response2.json().then((repo) => {
-              return repo;
-            })
-          }).then((repos) => {
-            var repoarray = repos.map((item) => {
-              return {
-                project_id: item.id,
-                name: item.name,
-                repourl: item.html_url,
-                description: item.description,
-                tasks: [],
-                follower: 0,
-                image: 'https://avatars1.githubusercontent.com/u/23557789?s=200&v=4'
-              }
-            });
-            return dispatch({type: 'GET_REPOS_FROM_GITHUB', projects: repoarray});
-          });
-        }));
-      });
-    });
-  };
-}
-
-export function getUserProject(token, username) {
-  return (dispatch) => {
-    return fetch('https://api.github.com/users/' + username + '/repos').then((response) => {
-      return response.json().then(function(json) {
-        var repoarray = json.map((item) => {
-          return {
-            project_id: item.id,
-            name: item.name,
-            repourl: item.html_url,
-            description: item.description,
-            tasks: [],
-            follower: 0,
-            image: 'https://avatars1.githubusercontent.com/u/23557789?s=200&v=4'
-          }
-        });
-        return dispatch({type: 'GET_REPOS_FROM_GITHUB', projects: repoarray});
-      });
-    });
-  };
-}
+// export function getOrgsProject(token) {
+//   return (dispatch) => {
+//     return fetch('https://api.github.com/user/orgs', {
+//       method: 'get',
+//       headers: {
+//         'Authorization': 'token ' + token
+//       }
+//     }).then((response) => {
+//       return response.json().then(function(orgs) {
+//         var repoarray = [];
+//         Promise.all(orgs.map((item) => {
+//           return fetch(item.repos_url).then((response2) => {
+//             return response2.json().then((repo) => {
+//               return repo;
+//             })
+//           }).then((repos) => {
+//             var repoarray = repos.map((item) => {
+//               return {
+//                 project_id: item.id,
+//                 name: item.name,
+//                 repourl: item.html_url,
+//                 description: item.description,
+//                 tasks: [],
+//                 follower: 0,
+//                 image: 'https://avatars1.githubusercontent.com/u/23557789?s=200&v=4'
+//               }
+//             });
+//             return dispatch({type: 'GET_REPOS_FROM_GITHUB', projects: repoarray});
+//           });
+//         }));
+//       });
+//     });
+//   };
+// }
+//
+// export function getUserProject(token, username) {
+//   return (dispatch) => {
+//     return fetch('https://api.github.com/users/' + username + '/repos').then((response) => {
+//       return response.json().then(function(json) {
+//         var repoarray = json.map((item) => {
+//           return {
+//             project_id: item.id,
+//             name: item.name,
+//             repourl: item.html_url,
+//             description: item.description,
+//             tasks: [],
+//             follower: 0,
+//             image: 'https://avatars1.githubusercontent.com/u/23557789?s=200&v=4'
+//           }
+//         });
+//         return dispatch({type: 'GET_REPOS_FROM_GITHUB', projects: repoarray});
+//       });
+//     });
+//   };
+// }
 
 export function updateProject(project) {
   return (dispatch) => {
@@ -224,7 +224,6 @@ export function getGithubProjects(user) {
         });
       }));
     }).then((repos) => {
-      //TODO diese beiden repos sind anders
       console.log("repos", repos)
       return dispatch({
         type: 'GET_GITREPOS_SUCCESSFUL',
@@ -238,7 +237,6 @@ export function getGithubProjects(user) {
     }).then((response) => {
       return response.json();
     }).then(function(repos) {
-      //hier kann dann alles gefiltert werden
       return dispatch({type: 'GET_PROJECT_LIST_SUCCESSFUL', projectList: repos});
     });
   };
