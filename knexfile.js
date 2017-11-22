dabfaddb68103de3c5272c011ca78e8f240ce2d2
Gott50 <xxGott50xx@gmail.com>
2017-11-22 09:29:10 +0100

    use pg in production

--- knexfile.js
+++ knexfile.js
@@ -18,19 +18,8 @@ let knexfile = {
         }
     },
     production: {
-        client: 'mssql',
-        connection: {
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            server: process.env.DB_SERVER,
-            database: process.env.DB_DATABASE,
-            options: {
-                encrypt: true,
-            },
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
+        client: 'pg',
+        connection: process.env.DATABASE_URL
     }
 };
 module.exports = knexfile[env];
