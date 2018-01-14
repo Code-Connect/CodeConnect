4131baa8e466ee533da20334b6e7d466e124698d
Gott50 <xxGott50xx@gmail.com>
2018-01-14 15:20:50 +0100

    cleanup

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
 import {IndexLink, Link} from "react-router";
-import {LinkContainer} from "react-bootstrap";
 
 
 class Header extends React.Component {
@@ -16,29 +15,27 @@ class Header extends React.Component {
         const rightNav = this.props.user
             ? (
                 <div className="menubarright">
-                <ul className="nav navbar-nav navbar-right">
-                    <li className="dropdown">
-                        <a href="#" data-toggle="dropdown" className="dropdown-toggle">
-                            <i className="caret"></i>
-                            <h2 className="title_menu_element right">
-                                {' '}{this.props.user.name || this.props.user.email || this.props.user.id}{' '}
-                            </h2>
-                            <img className="div-block" src={'https://avatars.githubusercontent.com/u/' + this.props.user.id}/>
-                        </a>
-                        <ul className="dropdown-menu">
-                            <li>
-                                <Link to="/profile">Profile</Link>
-                            </li>
-                            <li className="divider"></li>
-                            <li>
-                                <a href="/logout">Log out</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-
-                    {/*<h2 className="title_menu_element right">Tash Sultana</h2>*/}
-                    {/*<div className="div-block"></div>*/}
+                    <ul className="nav navbar-nav navbar-right">
+                        <li className="dropdown">
+                            <a href="#" data-toggle="dropdown" className="dropdown-toggle">
+                                <i className="caret"></i>
+                                <h2 className="title_menu_element right">
+                                    {' '}{this.props.user.name || this.props.user.email || this.props.user.id}{' '}
+                                </h2>
+                                <img className="div-block"
+                                     src={'https://avatars.githubusercontent.com/u/' + this.props.user.id}/>
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <Link to="/profile">Profile</Link>
+                                </li>
+                                <li className="divider"></li>
+                                <li>
+                                    <a href="/logout">Log out</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
             )
             : (
@@ -63,8 +60,6 @@ class Header extends React.Component {
             : (
                 <div></div>
             );
-        const navbarStyle = {backgroundColor: "#115180"};
-
         return (
             <nav className="menubar navbar navbar-default navbar-static-top navFont">
                 <div className="menubarleft">
@@ -81,8 +76,6 @@ class Header extends React.Component {
                     </div>
                     {mentorNav}
                 </div>
-
-
                 {rightNav}
             </nav>
         );
