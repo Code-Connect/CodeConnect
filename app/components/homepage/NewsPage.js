1cf27830d1d1c885f777384406ff486d37d914d1
Gott50 <xxGott50xx@gmail.com>
2018-01-14 14:10:36 +0100

    add Style for .cardheader

--- app/components/homepage/NewsPage.js
+++ app/components/homepage/NewsPage.js
@@ -25,6 +25,18 @@ class NewsPage extends Component {
                 <h1 className="titlepage container">Dashboard</h1>
             </div>
             <div className="body">
+                <div className="cardheader">
+                    <div className="third leftthird">
+                        <h3 className="cardtitle clicked">Projects</h3>
+                        <h2 className="cardtitle">Contributors</h2></div>
+                    <div className="third middlethird">
+                        <a href="#" className="cardtitle button left newest_hottest_clicked w-button">Newest</a>
+                        <a href="#" className="cardtitle button right w-button">Hottest</a>
+                    </div>
+                    <div className="third rightthird">
+                        <div className="searchicon"></div>
+                        <h2 className="cardtitle search">Search...</h2></div>
+                </div>
                 <div className="show-grid container">
                     {this.props.projects.projectList.map((project) => {
                         return (
